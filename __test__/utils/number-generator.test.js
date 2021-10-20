fdd02e8514e6aef89d05213dbd180f01798afd5d
JuanMa Ruiz <ruizjuanma@gmail.com>
2021-10-20 20:11:46 +0200

    (feat) configure eslint-jest linter
    * use eslint-plugin-jest
    * add specific configuration to .eslintrc.js file

--- __test__/utils/number-generator.test.js
+++ __test__/utils/number-generator.test.js
@@ -5,26 +5,26 @@ test('should generate a number', () => {
 });
 
 test('should generate a number of 8 digits by default', () => {
-  expect(getEightOrSevenDigitsNumber().toString().length).toBe(8);
+    expect(getEightOrSevenDigitsNumber().toString()).toHaveLength(8);
 });
 
 test('should generate a number with the passed number of digits', () => {
-  expect(getEightOrSevenDigitsNumber(8).toString().length).toBe(8);
-  expect(getEightOrSevenDigitsNumber(7).toString().length).toBe(7);
+    expect(getEightOrSevenDigitsNumber(8).toString()).toHaveLength(8);
+    expect(getEightOrSevenDigitsNumber(7).toString()).toHaveLength(7);
 });
 
 test('getEightOrSevenDigitsNumber should return a number of 7 digits', () => {
-  let n = 8;
-  while ( n > 0 ) {
-    expect(getEightOrSevenDigitsNumber(7).toString().length).toBe(7);
-    n--;
-  }
+    let n = 8;
+    while ( n > 0 ) {
+        expect(getEightOrSevenDigitsNumber(7).toString()).toHaveLength(7);
+        n--;
+    }
 });
 test('generate number should return a value between 0 an 2', () => {
-  let n = 5;
-  while ( n > 0 ) {
-    const num = generateNumber();
-    expect((num >= 0 || num < 3)).toBe(true);
-    n--;
-  }
+    let n = 5;
+    while ( n > 0 ) {
+        const num = generateNumber();
+        expect((num >= 0 || num < 3)).toBe(true);
+        n--;
+    }
 });
